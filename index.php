0ea56f8f5e0a096fdb5a76ef72143a085e43127d
dedyteguhs <teguhjawa@yahoo.co.id>
2019-09-22 21:28:53 +0700

    tambah index

--- index.php
+++ index.php
@@ -6,18 +6,31 @@ include_once "library/inc.library.php";
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title> :: Butik INDAH - Way Jepara Lampung Timur</title>
+<title> Bengkel Motor CITRA </title>
 <link href="styles/style.css" rel="stylesheet" type="text/css">
 <link type="text/css" rel="stylesheet" href="plugins/dhtmlgoodies_calendar/dhtmlgoodies_calendar/dhtmlgoodies_calendar.css" media="screen" />
 <script type="text/javascript" src="plugins/dhtmlgoodies_calendar/dhtmlgoodies_calendar/dhtmlgoodies_calendar.js"></script> 
 <script type="text/javascript">
 	bkLib.onDomLoaded(function() { nicEditors.allTextAreas() });
-</script></head>
+</script>
+<style>
+#header2{
+	height:100px;
+	width:100%;
+	overflow:hidden;
+	margin:0px;
+	padding:0px; 
+	background-image:url(logoterbaru.png); 
+	background-repeat:no-repeat; 
+	border-bottom:5px solid #DDDDDD;
+}
+</style>
+</head>
 <div id="wrap">
 <body>
 <table width="100%" class="table-main">
   <tr>
-    <td height="103" colspan="2"><a href="?page"><div id="header"></div>
+    <td height="103" colspan="2"><a href="?page"><div id="header2"></div>
     </a></td>
   </tr>
   <tr valign="top">
@@ -28,3 +41,5 @@ include_once "library/inc.library.php";
 </body>
 </div>
 </html>
+
+
